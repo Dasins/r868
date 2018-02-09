7365378c106927738dfac65dce3cad4043efae00
Dasins <dm.evilart@gmail.com>
2018-02-09 15:18:17 +0100

    Clase alumno, almacena tres notas, calcula su media y comprueba que todas esten aprobadas

---
+++ Alumno.java
@@ -0,0 +1,39 @@
+
+/**
+ * Esta clase simula alumnos con tres notas.
+ * Es capaz de calcular la nota media del alumno y determinar si tiene alguna asignatura pendiente. 
+ *
+ * @author D4s1ns
+ * @version 2018/02/09
+ */
+class Alumno {
+    String nombreCompleto;
+    int notaLengua;
+    int notaMates;
+    int notaIngles;
+    double notaMedia;
+    
+    public Alumno (String nombreCompleto, int notaLengua, int notaMates, int notaIngles) {
+        this.nombreCompleto = nombreCompleto;
+        this.notaLengua = notaLengua;
+        this.notaMates = notaMates;
+        this.notaIngles = notaIngles;
+        double notaMedia = calcularMedia();
+    }
+    
+    public double calcularMedia() {
+        return (notaLengua + notaMates + notaIngles) / 3;
+    }
+    
+    public double getNotaMedia() {
+        return notaMedia;
+    }
+    
+    public boolean comprobarTodoAprobado() {
+        boolean todoAprobado = false;
+        if(notaLengua >= 5 && notaMates >= 5 && notaIngles >= 5) {
+            todoAprobado = true;
+        }
+        return todoAprobado;
+    }
+}
